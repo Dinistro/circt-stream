befae6330daf3a37bc4baffac527bcf1976e56db
Christian Ulmann <christianulmann@gmail.com>
2022-04-27 17:11:53 +0200

    [StreamToHandshake] add lowerings for FuncOp and ReturnOp

--- lib/Conversion/StreamToHandshake/StreamToHandshake.cpp
+++ lib/Conversion/StreamToHandshake/StreamToHandshake.cpp
@@ -15,6 +15,7 @@
 #include "../PassDetail.h"
 #include "Standalone/Dialect/Stream/StreamDialect.h"
 #include "Standalone/Dialect/Stream/StreamOps.h"
+#include "circt/Conversion/StandardToHandshake.h"
 #include "circt/Dialect/Handshake/HandshakeDialect.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
@@ -24,6 +25,7 @@
 #include "mlir/Transforms/DialectConversion.h"
 
 using namespace circt;
+using namespace circt::handshake;
 using namespace mlir;
 using namespace standalone;
 using namespace stream;
@@ -35,6 +37,107 @@ class StreamTypeConverter : public TypeConverter {
   StreamTypeConverter() {
     addConversion([](Type type) { return type; });
     addConversion([](StreamType type) { return type.getElementType(); });
+  }
+};
+
+// Functionality to share state when lowering, see CIRCT's HandshakeLowering
+class StreamLowering : public HandshakeLowering {
+ public:
+  explicit StreamLowering(Region &r) : HandshakeLowering(r) {}
+
+  virtual LogicalResult setControlOnlyPath(
+      ConversionPatternRewriter &rewriter) override {
+    if (failed(HandshakeLowering::setControlOnlyPath(rewriter)))
+      return failure();
+
+    Block *entryBlock = &r.front();
+    Value ctrl = getBlockEntryControl(entryBlock);
+
+    // Replace original return ops with new returns with additional control
+    // input
+    for (auto yieldOp : llvm::make_early_inc_range(r.getOps<StreamYieldOp>())) {
+      rewriter.setInsertionPoint(yieldOp);
+      SmallVector<Value, 8> operands(yieldOp->getOperands());
+      operands.push_back(ctrl);
+      rewriter.replaceOpWithNewOp<handshake::ReturnOp>(yieldOp, operands);
+    }
+
+    return success();
+  }
+
+  virtual LogicalResult test(ConversionPatternRewriter &rewriter) {
+    return success();
+  }
+};
+
+struct FuncOpLowering : public OpConversionPattern<func::FuncOp> {
+  using OpConversionPattern<func::FuncOp>::OpConversionPattern;
+
+  LogicalResult matchAndRewrite(
+      func::FuncOp op, OpAdaptor adapter,
+      ConversionPatternRewriter &rewriter) const override {
+    // type conversion
+    TypeConverter *typeConverter = getTypeConverter();
+    FunctionType oldFuncType = op.getFunctionType().cast<FunctionType>();
+
+    TypeConverter::SignatureConversion sig(oldFuncType.getNumInputs());
+    SmallVector<Type, 1> newResults;
+    SmallVector<Type, 1> newArgs;
+    if (failed(
+            typeConverter->convertSignatureArgs(oldFuncType.getInputs(), sig)))
+      return failure();
+
+    // add the ctrl input
+    sig.addInputs({rewriter.getNoneType()});
+    if (failed(typeConverter->convertTypes(oldFuncType.getResults(),
+                                           newResults)) ||
+        failed(
+            rewriter.convertRegionTypes(&op.getBody(), *typeConverter, &sig)))
+      return failure();
+
+    newResults.push_back(rewriter.getNoneType());
+    auto newFuncType =
+        rewriter.getFunctionType(sig.getConvertedTypes(), newResults);
+
+    SmallVector<NamedAttribute, 4> attributes;
+    for (const auto &attr : op->getAttrs()) {
+      if (attr.getName() == SymbolTable::getSymbolAttrName() ||
+          attr.getName() == function_interface_impl::getTypeAttrName())
+        continue;
+      attributes.push_back(attr);
+    }
+
+    auto newFuncOp = rewriter.create<handshake::FuncOp>(
+        op.getLoc(), op.getName(), newFuncType, attributes);
+    rewriter.inlineRegionBefore(op.getBody(), newFuncOp.getBody(),
+                                newFuncOp.end());
+
+    rewriter.eraseOp(op);
+
+    // TODO cannot use that here, due to changed block argument types
+    // HandshakeLowering fol(newFuncOp.getBody());
+    // if (failed(lowerRegion(fol, false, false))) return failure();
+    newFuncOp.resolveArgAndResNames();
+
+    return success();
+  }
+};
+
+struct ReturnOpLowering : public OpConversionPattern<func::ReturnOp> {
+  using OpConversionPattern<func::ReturnOp>::OpConversionPattern;
+
+  LogicalResult matchAndRewrite(
+      func::ReturnOp op, OpAdaptor adaptor,
+      ConversionPatternRewriter &rewriter) const override {
+    // TODO find a cleaner way to do that
+    SmallVector<Value, 4> operands;
+    for (Value v : adaptor.getOperands()) operands.push_back(v);
+
+    Value ctrl = *(rewriter.getBlock()->args_rbegin());
+    assert(ctrl.getType().isa<NoneType>());
+    operands.push_back(ctrl);
+    rewriter.replaceOpWithNewOp<handshake::ReturnOp>(op, operands);
+    return success();
   }
 };
 
@@ -81,7 +184,9 @@ static void populateStreamToHandshakePatterns(
     StreamTypeConverter &typeConverter, RewritePatternSet &patterns) {
   // clang-format off
   patterns.add<
-    StreamMapLowering
+    StreamMapLowering,
+    FuncOpLowering,
+    ReturnOpLowering
   >(typeConverter, patterns.getContext());
   // clang-format on
 }
@@ -94,31 +199,11 @@ class StreamToHandshakePass
     StreamTypeConverter typeConverter;
     ConversionTarget target(getContext());
 
-    // Converts the types used in operations of the func dialect
-    populateFunctionOpInterfaceTypeConversionPattern<FuncOp>(patterns,
-                                                             typeConverter);
-    target.addDynamicallyLegalOp<FuncOp>([&](FuncOp op) {
-      return typeConverter.isSignatureLegal(op.getFunctionType()) &&
-             typeConverter.isLegal(&op.getBody());
-    });
-
-    populateCallOpTypeConversionPattern(patterns, typeConverter);
-    target.addDynamicallyLegalOp<func::CallOp>(
-        [&](func::CallOp op) { return typeConverter.isLegal(op); });
-
-    populateBranchOpInterfaceTypeConversionPattern(patterns, typeConverter);
-    populateReturnOpTypeConversionPattern(patterns, typeConverter);
-    target.addLegalOp<ModuleOp>();
-
-    target.markUnknownOpDynamicallyLegal([&](Operation *op) {
-      return isNotBranchOpInterfaceOrReturnLikeOp(op) ||
-             isLegalForBranchOpInterfaceTypeConversionPattern(op,
-                                                              typeConverter) ||
-             isLegalForReturnOpTypeConversionPattern(op, typeConverter);
-    });
-
     // Patterns to lower stream dialect operations
     populateStreamToHandshakePatterns(typeConverter, patterns);
+    target.addLegalOp<ModuleOp>();
+    target.addLegalDialect<handshake::HandshakeDialect>();
+    target.addIllegalDialect<func::FuncDialect>();
     target.addIllegalDialect<StreamDialect>();
 
     if (failed(
---
+++ test/Conversion/StreamToHandshake/noop.mlir
@@ -0,0 +1,10 @@
+// RUN: standalone-opt %s --convert-stream-to-handshake | FileCheck %s
+
+module {
+  func @noop(%in: !stream.stream<i32>) -> !stream.stream<i32> {
+    return %in : !stream.stream<i32>
+  }
+// CHECK: handshake.func @noop(%[[VAL:.*]]: i32, %[[CTRL:.*]]: none, ...) -> (i32, none) attributes {argNames = ["in0", "inCtrl"], resNames = ["out0", "outCtrl"]} {
+// CHECK-NEXT:    return %[[VAL]], %[[CTRL]] : i32, none
+// CHECK-NEXT:  }
+}
