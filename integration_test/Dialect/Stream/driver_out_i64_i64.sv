bcc2ed05e6d4846e7dabe3495bfbb7939e9babcc
Christian Ulmann <christianulmann@gmail.com>
2022-06-13 14:43:25 +0200

    [StreamToHandshake] Fix integration test drivers for new lowerings

--- integration_test/Dialect/Stream/driver_out_i64_i64.sv
+++ integration_test/Dialect/Stream/driver_out_i64_i64.sv
@@ -5,10 +5,12 @@ module driver();
   logic out0_valid, out0_ready;
   logic [63:0] out0_data_field0;
   logic out0_data_field1;
-
   logic out1_valid, out1_ready;
-  logic [63:0] out1_data_field0;
-  logic out1_data_field1;
+
+  logic out2_valid, out2_ready;
+  logic [63:0] out2_data_field0;
+  logic out2_data_field1;
+  logic out3_valid, out3_ready;
 
   logic inCtrl_valid, inCtrl_ready;
   logic outCtrl_valid, outCtrl_ready;
@@ -25,6 +27,8 @@ module driver();
     inCtrl_valid = 1;
     out0_ready = 1;
     out1_ready = 1;
+    out2_ready = 1;
+    out3_ready = 1;
     outCtrl_ready = 1;
 
     reset = 1;
@@ -51,8 +55,8 @@ module driver();
       end
     end
     if(out1_valid == 1 && s1_done == 0) begin
-      if(out1_data_field1 == 0) begin
-        $display("S1: Element=%d", out1_data_field0);
+      if(out2_data_field1 == 0) begin
+        $display("S1: Element=%d", out2_data_field0);
       end
       else begin
         $display("S1: EOS");
