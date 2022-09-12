9fdcc1b0abafbf90d7ac4bd8a7648f0bd337c154
Christian Ulmann <christianulmann@gmail.com>
2022-09-12 10:04:07 +0200

    [Stream][lit] Adapt old lit tests to match the new

--- integration_test/Dialect/Stream/projection-with-reg/projection-with-reg.py
+++ integration_test/Dialect/Stream/projection-with-reg/projection-with-reg.py
@@ -1,79 +1,43 @@
 import cocotb
-import cocotb.clock
-from cocotb.triggers import FallingEdge, RisingEdge
-
-# Hack to allow imports from parent directory
-import sys
-import os
-from helper import HandshakePort, getPorts, Stream
+from helper import initDut, Stream
 import random
 
 
-async def initDut(dut):
-  [in0, in1, inCtrl], [out0, out1,
-                       outCtrl] = getPorts(dut, ["in0", "in1", "inCtrl"],
-                                           ["out0", "out1", "outCtrl"])
+async def initStreams(dut):
+  ins, outs = await initDut(dut)
 
-  # Create a 10us period clock on port clock
-  clock = cocotb.clock.Clock(dut.clock, 10, units="us")
-  cocotb.start_soon(clock.start())  # Start the clock
-
-  in0.setValid(0)
-  in1.setValid(0)
-  inCtrl.setValid(0)
-
-  out0.setReady(1)
-  out1.setReady(1)
-  outCtrl.setReady(1)
-
-  # Reset
-  dut.reset.value = 1
-  await RisingEdge(dut.clock)
-  dut.reset.value = 0
-  await RisingEdge(dut.clock)
-
-  # init circuit
-  inCtrlSend = cocotb.start_soon(inCtrl.send())
-  await inCtrlSend
-
-  inStream = Stream(in0, in1)
-  outStream = Stream(out0, out1)
-  return inStream, outStream
-
+  in0 = Stream(ins[0], ins[1])
+  out0 = Stream(outs[0], outs[1])
+  return in0, out0
 
 @cocotb.test()
 async def increase(dut):
-  inStream, outStream = await initDut(dut)
+  inStream, outStream = await initStreams(dut)
 
   resCheck = cocotb.start_soon(outStream.checkOutputs([(2, 2), (4, 4),
                                                        (6, 6)]))
 
-  inputs = [(1, 1), (2, 2), (3, 3)]
-  for i in inputs:
-    await inStream.sendData(i)
-  await inStream.sendEOS()
+  cocotb.start_soon(inStream.sendAndTerminate([(1, 1), (2, 2), (3, 3)]))
 
   await resCheck
 
 
 @cocotb.test()
 async def mixed(dut):
-  inStream, outStream = await initDut(dut)
+  inStream, outStream = await initStreams(dut)
 
   resCheck = cocotb.start_soon(
       outStream.checkOutputs([(11, 11), (4, 11), (30, 30), (29, 30)]))
 
   inputs = [(1, 10), (2, 2), (30, 0), (15, 14)]
-  for i in inputs:
-    await inStream.sendData(i)
-  await inStream.sendEOS()
+  cocotb.start_soon(inStream.sendAndTerminate(inputs))
 
   await resCheck
 
 
 @cocotb.test()
 async def randomized(dut):
-  inStream, outStream = await initDut(dut)
+  inStream, outStream = await initStreams(dut)
 
   N = 100
 
@@ -87,9 +51,6 @@ async def randomized(dut):
     outputs.append((s, m))
 
   resCheck = cocotb.start_soon(outStream.checkOutputs(outputs))
-
-  for i in inputs:
-    await inStream.sendData(i)
-  await inStream.sendEOS()
+  cocotb.start_soon(inStream.sendAndTerminate(inputs))
 
   await resCheck
