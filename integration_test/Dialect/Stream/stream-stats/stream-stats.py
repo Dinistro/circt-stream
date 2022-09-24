0afbe55c21a4d1cd7fd8f41258e36b1517672f18
Christian Ulmann <christianulmann@gmail.com>
2022-09-24 15:01:19 +0200

    [Stream][Lit] Add stream stats test

---
+++ integration_test/Dialect/Stream/stream-stats/stream-stats.py
@@ -0,0 +1,47 @@
+import cocotb
+from helper import initDut, Stream
+import random
+
+
+@cocotb.test()
+async def sendOne(dut):
+  ins, outs = await initDut(dut)
+
+  in0 = Stream(ins[0], ins[1])
+  out0 = Stream(outs[0], outs[1])
+  out1 = Stream(outs[2], outs[3])
+
+  inputs = [(10,2,3,6,5,1,5,9)]
+  outputs = [max([max(list(i)) for i in inputs])]
+
+  out0Check = cocotb.start_soon(out0.checkOutputs(inputs))
+  out1Check = cocotb.start_soon(out1.checkOutputs(outputs))
+
+  cocotb.start_soon(in0.sendAndTerminate(inputs))
+
+  await out0Check
+  await out1Check
+
+def randomTuple():
+  return tuple([random.randint(0, 100) for _ in range(8)])
+
+
+@cocotb.test()
+async def sendMultiple(dut):
+  ins, outs = await initDut(dut)
+
+  in0 = Stream(ins[0], ins[1])
+  out0 = Stream(outs[0], outs[1])
+  out1 = Stream(outs[2], outs[3])
+
+  N = 100
+  inputs = [randomTuple() for _ in range(N)]
+  outputs = [max([max(list(i)) for i in inputs])]
+
+  out0Check = cocotb.start_soon(out0.checkOutputs(inputs))
+  out1Check = cocotb.start_soon(out1.checkOutputs(outputs))
+
+  cocotb.start_soon(in0.sendAndTerminate(inputs))
+
+  await out0Check
+  await out1Check
