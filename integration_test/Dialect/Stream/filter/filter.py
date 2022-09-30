cc015ffaab7e7ccc04eab0a2dc9b26a94bcd0d46
Christian Ulmann <christianulmann@gmail.com>
2022-09-30 09:58:15 +0200

    [StreamToHandshake] Reset registers on EOS for reuse (#83)
    This change ensures not only that lambda is not executed on EOS, but
    additionally resets the registers on EOS.

--- integration_test/Dialect/Stream/filter/filter.py
+++ integration_test/Dialect/Stream/filter/filter.py
@@ -14,3 +14,17 @@ async def ascendingInputs(dut):
   cocotb.start_soon(in0.sendAndTerminate([0,1,2,0,4,0]))
 
   await out0Check
+
+@cocotb.test()
+async def multipleEOS(dut):
+  ins, outs = await initDut(dut)
+
+  in0 = Stream(ins[0], ins[1])
+  out0 = Stream(outs[0], outs[1])
+
+  for _ in range(5):
+    out0Check = cocotb.start_soon(out0.checkOutputs([1,2,4]))
+
+    cocotb.start_soon(in0.sendAndTerminate([0,1,2,0,4,0]))
+
+    await out0Check
